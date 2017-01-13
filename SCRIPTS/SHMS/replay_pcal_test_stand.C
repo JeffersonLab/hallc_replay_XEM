1519d8ad1ddcca900393d99af8ba40152b4103d5
Eric Pooser <pooser@jlab.org>
2017-01-13 15:22:01 -0500

    Incorporate SHMS pre-shower and shower in the hallc replay framework

--- SCRIPTS/SHMS/replay_pcal_test_stand.C
+++ SCRIPTS/SHMS/replay_pcal_test_stand.C
@@ -1,4 +1,4 @@
-void replay_hcal_test_stand(Int_t RunNumber=0, Int_t MaxEvent=0) {
+void replay_pcal_test_stand(Int_t RunNumber=0, Int_t MaxEvent=0) {
 
   // Get RunNumber and MaxEvent if not provided.
   if(RunNumber == 0) {
@@ -16,8 +16,8 @@ void replay_hcal_test_stand(Int_t RunNumber=0, Int_t MaxEvent=0) {
   }
 
   // Create file name patterns.
-  const char* RunFileNamePattern = "raw/hms_all_%05d.dat";
-  const char* ROOTFileNamePattern = "ROOTfiles/hms_hcal_replay_%d.root";
+  const char* RunFileNamePattern = "raw/shms_all_%05d.dat";
+  const char* ROOTFileNamePattern = "ROOTfiles/shms_pcal_replay_%d.root";
   // Add variables to global list.
   gHcParms->Define("gen_run_number", "Run Number", RunNumber);
   gHcParms->AddString("g_ctp_database_filename", "DBASE/standard.database");
@@ -31,14 +31,14 @@ void replay_hcal_test_stand(Int_t RunNumber=0, Int_t MaxEvent=0) {
 
   // Load the Hall C style detector map
   gHcDetectorMap = new THcDetectorMap();
-  gHcDetectorMap->Load("MAPS/HMS/DETEC/hcal.map");
+  gHcDetectorMap->Load("MAPS/SHMS/DETEC/pcal.map");
   
   // Set up the equipment to be analyzed.
-  THaApparatus* HMS = new THcHallCSpectrometer("H", "HMS");
-  gHaApps->Add(HMS);
+  THaApparatus* SHMS = new THcHallCSpectrometer("P", "SHMS");
+  gHaApps->Add(SHMS);
   // Add hodoscope to HMS apparatus
   THcShower* cal = new THcShower("cal", "Calorimeter");
-  HMS->AddDetector(cal);
+  SHMS->AddDetector(cal);
 
   // Set up the analyzer - we use the standard one,
   // but this could be an experiment-specific one as well.
@@ -74,8 +74,8 @@ void replay_hcal_test_stand(Int_t RunNumber=0, Int_t MaxEvent=0) {
  analyzer->SetEvent(event);
  analyzer->SetCrateMapFileName("MAPS/db_cratemap.dat");
  analyzer->SetOutFile(ROOTFileName.Data());
- analyzer->SetOdefFile("DEF-files/HMS/CAL/hcalana.def");
- analyzer->SetCutFile("DEF-files/HMS/CAL/hcalana_cuts.def");    // optional
+ analyzer->SetOdefFile("DEF-files/SHMS/CAL/pcalana.def");
+ analyzer->SetCutFile("DEF-files/SHMS/CAL/pcalana_cuts.def");    // optional
 
  // File to record cuts accounting information
  //analyzer->SetSummaryFile("summary_example.log");    // optional
