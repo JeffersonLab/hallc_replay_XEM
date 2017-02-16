f721b198388bf30942f51df7a7cf94a98f90675e
Eric Pooser <pooser@jlab.org>
2017-02-16 10:01:52 -0500

    Minor commit with minimal changes

--- onlineGUI/Macros/kpp_analysis.C
+++ onlineGUI/Macros/kpp_analysis.C
@@ -1,27 +1,34 @@
 
 void UserScript() {
 
-  UInt_t num_p1T;
+  Double_t num_p1T;
 
   Double_t p1T_tdc[128];
+
+  Long64_t nentries;
 
   TH1F *h_p1T_tdc;
 
   TTree *T = (TTree*) gDirectory->Get("T");
 
+  nentries = T->GetEntries();
+
   T->SetBranchAddress("T.shms.p1T_tdcMultiplicity", &num_p1T);
   T->SetBranchAddress("T.shms.p1T_tdcTime", &p1T_tdc);
 
-  h_p1T_tdc = new TH1F("h_p1T_tdc", "p1T; TDC Time (ns); Counts / 1 ns", 200, 0, 200);
+  h_p1T_tdc = new TH1F("h_p1T_tdc", "p1T; TDC Time (ns); Counts / 1 ns", 2000, 0, 2000);
 
-  for(UInt_t iev = 0, N = T->GetEntries(); iev < N; iev++) {
-    T->GetEntry(iev);
+  for(UInt_t ievent = 0; ievent < nentries; ievent++) {
+    
+    T->GetEntry(ievent);
 
-    for (Uint_t ihit = 0; i < nump1T; ihit++) {
+    for (UInt_t ihit = 0; ihit < num_p1T; ihit++) {
 
-      Double_t p1T_tdc_data = p1T_tdc[ihit++];
+      Double_t p1T_tdc_data = p1T_tdc[ihit];
 
-      h_p1T_tdc->Fill(p1T_tdc);
+      cout << ihit << "\t" << p1T_tdc_data << endl;
+
+      h_p1T_tdc->Fill(p1T_tdc_data);
 
       }
 
@@ -31,9 +38,7 @@ void UserScript() {
 
 void kpp_analysis(TString histname) {
 
-  TH1F* h;
-  h = (TH1F*) gDirectory->Get(histname);
-  asdf f;
+  TH1F* h; h = (TH1F*) gDirectory->Get(histname);
   if(!h) {
     UserScript();
     h = (TH1F*) gDirectory->Get(histname);
