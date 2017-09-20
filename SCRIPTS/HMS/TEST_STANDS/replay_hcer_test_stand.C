b342e543b1fb1970b734a7111d56d1bb9e3db5cb
Brad Sawatzky <sawatzky@users.noreply.github.com>
2017-09-20 09:33:36 -0400

    Find coda files in a path list (#209)
    * Add vim lint to .gitignore
    
    * Update replay scripts to search in a list of paths
    
    - NOTE: This requires the podd submodule be updated to include
      modifcations to the THaRun constructor that can search a pathList.
    
    - The default './raw' and './cache' pathnames that are searched here
      should generally be symlinks to the relevant local path on the Hall C
      cluster, and the /cache/mss/hallc/.../raw/ path, respectively. For
      example:
        cd hallc_replay
        ln -s /net/cdaq/cdaql3data/coda/data/raw raw
        ln -s /cache/mss/hallc/spring17/raw      cache
    
    - FWIW: The batch filtering of the replay scripts was done with
      this perl script:
        cd SCRIPTS/
        find . -name '*.C' -exec ./filter.pl {} \;
      --- filter.pl ---
      #!/usr/bin/perl
    
      use File::Slurp;
    
      my $file = read_file($ARGV[0]);
    
      $file =~ s/\Qchar RunFileName[100];\E\W*\Qsprintf(RunFileName, RunFileNamePattern, RunNumber);\E\W*\QTHaRun* run = new THaRun(RunFileName);\E/THaRun* run = new THaRun( pathList, Form(RunFileNamePattern, RunNumber) );/s;
    
      $file =~ s/\Qconst char* RunFileNamePattern = "\E.*?\/(.*?\.dat)";/const char* RunFileNamePattern = "$1";\n  vector<TString> pathList;\n    pathList.push_back(".");\n    pathList.push_back(".\/raw");\n    pathList.push_back(".\/cache");\n/s;
    
      write_file("$ARGV[0]", $file);
      ---

--- SCRIPTS/HMS/TEST_STANDS/replay_hcer_test_stand.C
+++ SCRIPTS/HMS/TEST_STANDS/replay_hcer_test_stand.C
@@ -16,7 +16,12 @@ void replay_hcer_test_stand(Int_t RunNumber=0, Int_t MaxEvent=0) {
   }
 
   // Create file name patterns.
-  const char* RunFileNamePattern = "raw/hms_all_%05d.dat";
+  const char* RunFileNamePattern = "hms_all_%05d.dat";
+  vector<TString> pathList;
+    pathList.push_back(".");
+    pathList.push_back("./raw");
+    pathList.push_back("./cache");
+
   const char* ROOTFileNamePattern = "ROOTfiles/hcer_replay_%d.root";
 
   // Add variables to global list.
@@ -68,9 +73,7 @@ void replay_hcer_test_stand(Int_t RunNumber=0, Int_t MaxEvent=0) {
 
   // Define the run(s) that we want to analyze.
   // We just set up one, but this could be many.
-  char RunFileName[100];
-  sprintf(RunFileName, RunFileNamePattern, RunNumber);
-  THaRun* run = new THaRun(RunFileName);
+  THaRun* run = new THaRun( pathList, Form(RunFileNamePattern, RunNumber) );
 
   // Eventually need to learn to skip over, or properly analyze
   // the pedestal events
