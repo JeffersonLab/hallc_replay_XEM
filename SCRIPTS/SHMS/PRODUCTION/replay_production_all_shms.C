63c31b47d8c37e26c552e259a04203d6e7f12167
Casey Morean <cmorean@jlab.org>
2021-02-05 12:46:08 -0500

    Add slight changes to pcana for sp18.

--- SCRIPTS/SHMS/PRODUCTION/replay_production_all_shms.C
+++ SCRIPTS/SHMS/PRODUCTION/replay_production_all_shms.C
@@ -31,6 +31,7 @@ void replay_production_all_shms (Int_t RunNumber = 0, Int_t MaxEvent = 0) {
   gHcParms->Load(gHcParms->GetString("g_ctp_database_filename"), RunNumber);
   gHcParms->Load(gHcParms->GetString("g_ctp_parm_filename"));
   gHcParms->Load(gHcParms->GetString("g_ctp_kinematics_filename"), RunNumber);
+  gHcParms->Load(gHcParms->GetString("g_ctp_calib_filename"));
   // Load parameters for SHMS trigger configuration
   gHcParms->Load("PARAM/TRIG/tshms.param");
   // Load fadc debug parameters
