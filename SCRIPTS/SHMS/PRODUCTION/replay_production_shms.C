83f1f0d3321bbb34525c341472a770a557475ae1
Tyler Hague <TJHague@gmail.com>
2025-02-04 14:12:25 -0500

    Final updates to replay as used for SHMS pass 1
    Pass 1 is done and good to go. This only has small changes to the output. The primary change was done in hcana to change the extended target corrections.

--- SCRIPTS/SHMS/PRODUCTION/replay_production_shms.C
+++ SCRIPTS/SHMS/PRODUCTION/replay_production_shms.C
@@ -26,9 +26,9 @@ void replay_production_shms (Int_t RunNumber = 0, Int_t MaxEvent = 0) {
   // Create file name patterns.
   const char* RunFileNamePattern = "shms_all_%05d.dat";  //Raw data file name pattern
 //THESE ARE THE ORIGINALS  
-  // const char* ROOTFileNamePattern = "ROOTfiles/SHMS/PRODUCTION/shms_replay_production_%d_%d.root";
+  const char* ROOTFileNamePattern = "ROOTfiles/SHMS/PRODUCTION/shms_replay_production_%d_%d.root";
 //THESE ARE THE EXTRA ONES FOR OFFSET STUDY
-  const char* ROOTFileNamePattern = "tempOUTPUT/shms_replay_production_%d_%d.root";
+  // const char* ROOTFileNamePattern = "tempOUTPUT/shms_replay_production_%d_%d.root";
   TString ROOTFileName = Form(ROOTFileNamePattern, RunNumber, MaxEvent);
   //Specifics for the replay
   TString odef_file = "DEF-files/SHMS/PRODUCTION/pstackana_production.def";
@@ -37,15 +37,15 @@ void replay_production_shms (Int_t RunNumber = 0, Int_t MaxEvent = 0) {
 // IF the replay is for runs with preshower blocks turned OFF, use this one:
 //  TString cdef_file = "DEF-files/SHMS/PRODUCTION/CUTS/pstackana_production_cuts_xCalCuts.def";
 //THESE ARE THE ORIGINALS  
-  // TString summary_file = Form("REPORT_OUTPUT/SHMS/PRODUCTION/summary_production_%d_%d.report",
-	// 		      RunNumber, MaxEvent);
-  // TString report_file  = Form("REPORT_OUTPUT/SHMS/PRODUCTION/replay_shms_production_%d_%d.report",
-	// 		      RunNumber, MaxEvent);
+   TString summary_file = Form("REPORT_OUTPUT/SHMS/PRODUCTION/summary_production_%d_%d.report",
+	 		      RunNumber, MaxEvent);
+   TString report_file  = Form("REPORT_OUTPUT/SHMS/PRODUCTION/replay_shms_production_%d_%d.report",
+	 		      RunNumber, MaxEvent);
 //THESE ARE THE EXTRA ONES FOR OFFSET STUDY
-  TString summary_file = Form("tempOUTPUT/summary_production_%d_%d.report",
-			      RunNumber, MaxEvent);
-  TString report_file  = Form("tempOUTPUT/replay_shms_production_%d_%d.report",
-			      RunNumber, MaxEvent);
+  //TString summary_file = Form("tempOUTPUT/summary_production_%d_%d.report",
+	//		      RunNumber, MaxEvent);
+  //TString report_file  = Form("tempOUTPUT/replay_shms_production_%d_%d.report",
+	//		      RunNumber, MaxEvent);
 
   //Initialize gHcParms.
   //Shared SHMS gHcParms setup located in ../shms_shared.h
